59cc755d206babf1d18fdf191a3710fa2fcd0696
Matheus <epifanio.matheus@hotmail.com>
2025-08-23 14:23:37 -0300

    starting

---
+++ DigitalAgriculture/src/main/java/com/matheus/DigitalAgriculture/DigitalAgricultureApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.DigitalAgriculture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalAgricultureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalAgricultureApplication.class, args);
+	}
+
+}
