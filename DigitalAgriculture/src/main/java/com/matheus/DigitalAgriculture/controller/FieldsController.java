d3af1fa9906bce7d74e6736ec04a706f3523cf78
Matheus <epifanio.matheus@hotmail.com>
2025-08-29 19:34:25 -0300

    hotfix/save-userId: adjust endpoints to handle userId
    - Update retrieval of userId via AuthenticationPrincipal

--- DigitalAgriculture/src/main/java/com/matheus/DigitalAgriculture/controller/FieldsController.java
+++ DigitalAgriculture/src/main/java/com/matheus/DigitalAgriculture/controller/FieldsController.java
@@ -4,6 +4,7 @@ import com.matheus.DigitalAgriculture.dto.request.FieldsRequestDTO;
 import com.matheus.DigitalAgriculture.dto.response.FieldDetailsResponseDTO;
 import com.matheus.DigitalAgriculture.dto.response.FieldsResponseDTO;
 import com.matheus.DigitalAgriculture.dto.response.PaginationResponseDTO;
+import com.matheus.DigitalAgriculture.model.Users;
 import com.matheus.DigitalAgriculture.services.FieldsServices;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
@@ -11,6 +12,9 @@ import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,12 +26,11 @@ public class FieldsController {
     @Autowired
     FieldsServices fieldsServices;
 
-    @GetMapping("/{user_id}")
+    @GetMapping
     public PaginationResponseDTO<FieldsResponseDTO> findFieldsByUserId(@RequestParam(defaultValue = "0") @PositiveOrZero int numberPage,
-                                                                      @RequestParam(defaultValue = "10") @Positive int lengthPage,
-                                                                      @PathVariable @NotNull @Positive long user_id) {
-
-        return fieldsServices.findFieldsByUserId(numberPage, lengthPage, user_id);
+                                                                       @RequestParam(defaultValue = "10") @Positive int lengthPage,
+                                                                       @AuthenticationPrincipal Users users) {
+        return fieldsServices.findFieldsByUserId(numberPage, lengthPage, users.getId());
     }
 
     @PostMapping
